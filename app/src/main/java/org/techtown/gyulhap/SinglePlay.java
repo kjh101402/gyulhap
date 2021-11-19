2efa4b68d297bff7158ad9e28060b5bf806478b5
kjh101402 <kjh101402@naver.com>
2021-11-19 19:57:43 +0900

    Update SinglePlay.java
    랜덤 이미지 준비

--- app/src/main/java/org/techtown/gyulhap/SinglePlay.java
+++ app/src/main/java/org/techtown/gyulhap/SinglePlay.java
@@ -6,7 +6,16 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SinglePlay extends AppCompatActivity {
-
+    int[] pictures = {R.drawable.black_blue_circle, R.drawable.black_blue_square, R.drawable.black_blue_triangle,
+            R.drawable.black_green_circle, R.drawable.black_green_square, R.drawable.black_green_triangle,
+            R.drawable.black_red_circle, R.drawable.black_red_square, R.drawable.black_red_triangle,
+            R.drawable.gray_blue_circle, R.drawable.gray_blue_square, R.drawable.gray_blue_triangle,
+            R.drawable.gray_green_circle, R.drawable.gray_green_square, R.drawable.gray_green_triangle,
+            R.drawable.gray_red_circle, R.drawable.gray_red_square, R.drawable.gray_red_triangle,
+            R.drawable.white_blue_circle, R.drawable.white_blue_square, R.drawable.white_blue_triangle,
+            R.drawable.white_green_circle, R.drawable.white_green_square, R.drawable.white_green_triangle,
+            R.drawable.white_red_circle, R.drawable.white_red_square, R.drawable.white_red_triangle};
+    
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
